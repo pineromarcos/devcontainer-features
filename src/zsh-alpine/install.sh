eb79dff9e6b4a7693fdbbd5287faa6c78d72aa46
Marcos Piñero <pinero.marcos@gmail.com>
2025-06-04 21:54:46 -0300

    fix vcs_info

--- src/zsh-alpine/install.sh
+++ src/zsh-alpine/install.sh
@@ -20,4 +20,10 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/cust
 sed -i "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"${ZSH_THEME}\"/g" ~/.zshrc
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/g' ~/.zshrc
 
+echo "" >> ~/.zshrc
+echo "# Fix para vcs_info" >> ~/.zshrc
+echo "autoload -Uz vcs_info" >> ~/.zshrc
+echo "autoload -U compinit && compinit" >> ~/.zshrc
+echo "setopt prompt_subst" >> ~/.zshrc
+
 echo "OhMyZsh Installed"
